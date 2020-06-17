37d8058e0b142a38e147e6479d8dc47cbe94d78f
florentCrzt <florent.crozet@hotmail.fr>
2020-06-17 14:38:13 +0200

    Create basic class for Mnist model

--- 0_Mnist_classifer/0_Mnist_classifier.py
+++ 0_Mnist_classifer/0_Mnist_classifier.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+from model import KerasMnistModel
+
 
 #Download MNIST dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -13,9 +15,21 @@ x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
 input_shape = (28, 28, 1)
 
-x_train = x_train.as_type('float32')
-x_test = x_test.as_type('float32')
+#x_train = x_train.as_type('float32')
+#x_test = x_test.as_type('float32')
 
-x_train /= 255
-x_test /= 255
+#x_train /= 255
+#x_test /= 255
 
+y_train = tf.keras.utils.to_categorical(y_train)
+y_test = tf.keras.utils.to_categorical(y_test)
+
+
+#Prepare the model
+mnist_model = KerasMnistModel(_input_shape=input_shape,_output_shape=(10,1))
+
+mnist_model.printModel()
+
+mnist_model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"])
+
+mnist_model.train(x_train, y_train, batch_size= 8, epoch= 10)
--- 0_Mnist_classifer/model.py
+++ 0_Mnist_classifer/model.py
@@ -1,39 +1,35 @@
 import tensorflow as tf
 import numpy as np
 
-#Implementation of a neural network without keras
-class TensorflowMnistModel :
-    def __init__(self,_n_input, _n_output, _epoch, _batch_size, _lr):
-        self.n_input = _n_input
-        self.n_output = _n_output
-        self.epoch = _epoch
-        self.batch_size = _batch_size
-        self.lr = _lr
-
-
-    def buildModel(self, n_neurons_1):
-        X = tf.keras.backend.placeholder(tf.float32, [None, self.n_input])
-        Y = tf.keras.backend.placeholder(tf.float32, {None, self.n_output})
-
-        init1 = tf.initializers.TruncatedNormal((self.n_input, n_neurons_1), stddev= 2 / np.sqrt(self.n_input+n_neurons_1))
-        init2 = tf.initializers.TruncatedNormal((n_neurons_1, self.n_output), stddev= 2 / np.sqrt(self.n_output+n_neurons_1))
-        w1 = tf.Variable(init1)
-        w2 = tf.Variable(init2)
-        b1 = tf.Variable(np.zeros(n_neurons_1, dtype="float32"))
-        b2 = tf.Variable(np.zeros(self.n_output, dtype="float32"))
-
-        z1 = tf.matmul(X,w1) + b1
-        h1 = tf.nn.relu(z1)
-        z2 = tf.matmul(h1,w2) + b2
-        Y = tf.nn.relu(z2)
-
-
-#Implementation of the same neural network with keras
+#Implementation of a simple neural neural network with keras
 
 class KerasMnistModel :
-    def __init__(self,_n_input, _n_output, _epoch, _batch_size, _lr):
-        self.n_input = _n_input
-        self.n_output = _n_output
-        self.epoch = _epoch
-        self.batch_size = _batch_size
-        self.lr = _lr
+    def __init__(self,_input_shape, _output_shape):
+        self.input_shape = _input_shape
+        self.output_shape = _output_shape
+        self.model = self.buildModel()
+
+    def buildModel(self) :
+        image_input = tf.keras.Input(shape= self.input_shape)
+        x = tf.keras.layers.Flatten()(image_input)
+        x = tf.keras.layers.Dense(32)(x)
+        x = tf.keras.activations.sigmoid(x)
+        x = tf.keras.layers.Dense(10)(x)
+        x = tf.keras.activations.softmax(x)
+        model = tf.keras.Model(inputs= image_input, outputs= x)
+        return model
+    
+    def printModel(self):
+        self.model.summary()
+
+    def compile(self, optimizer, loss, metrics):
+        self.model.compile(optimizer= optimizer, loss= loss, metrics= metrics)
+    #Will be modify with generator
+    def train(self,x,y, batch_size, epoch):
+        self.model.fit(x,y,batch_size= batch_size, epochs= epoch)
+    
+    def save(self,path):
+        self.model.save(path)
+    
+    def load_model(self, path):
+        self.model.load_model(path)
